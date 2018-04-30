279a6a10884c89b29378e59885d46551546ab844
green <makotomidori.8912@gmail.com>
2018-04-30 17:04:41 +0900

    add password user-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_30_061207) do
+ActiveRecord::Schema.define(version: 2018_04_30_080300) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_04_30_061207) do
     t.string "mail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
